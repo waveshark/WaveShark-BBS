2218c1fd6ec75486152fb829bf69a039af6055b9
Dan D'Andrea <dan.dandrea@gmail.com>
2022-06-22 01:24:29 -0400

    Bring BBS up to latest ideas

--- ws-bbs.py
+++ ws-bbs.py
@@ -1,82 +1,133 @@
-import serial
+# TODO: HANDLE UNKNOWN COMMANDS
+# TODO: READ/WRITE WALL AND LAST HEARD FROM/TO DISK
+
+import sys
 from datetime import datetime, timedelta
+import time
+import re
+import serial
+import serial.tools.list_ports
 
-ser = serial.Serial("/dev/ttyUSB0", 115200, timeout = 0.01)
+def readLineFromSerial(ser):
+  return ser.readline().decode("ascii").strip()
 
+def writeToSerial(ser, str, numLinesToEat = 1):
+  ser.write(bytes("{}\r".format(str), "ascii"))
+  for i in range(0, numLinesToEat):
+    readLineFromSerial(ser)
+
+def try_connect():
+  for port, desc, hwid in sorted(serial.tools.list_ports.comports()):
+    if "CP2102" in desc:
+      try:
+        ser = serial.Serial("/dev/ttyUSB0", 115200, timeout = 0.1)
+        writeToSerial(ser, "/VERSION")
+        if "WaveShark firmware" in readLineFromSerial(ser):
+          return ser
+      except:
+        type, value, traceback = sys.exc_info()
+        print(value)
+        pass
+
+  return False
+
+# Connect to WaveShark Communicator
+ser = try_connect()
+if ser != False:
+  print("Connected to WaveShark Communicator device")
+else:
+  print("Failed to connect to WaveShark Communicator device")
+  exit()
+
+# Get device's sender name / device name
+writeToSerial(ser, "/NAME")
+bbs_name = readLineFromSerial(ser).split("[")[1].split("]")[0]
+print("BBS name: [{}]".format(bbs_name))
+
+# Configure device for BBS operation
+writeToSerial(ser, "/SEROUT FIELDTEST", 3)
+
+# For sending periodic BBS announcements
 announceSeconds = 600
 nextAnnounce = datetime.now()
 
-lastHeard = {}
+# Wall and last heard
+max_wall_messages = 3
 wall = []
+lastHeard = {}
 
-def writeToSerial(s):
-  ser.write(bytes(s, "ascii"))
-
+# Process incoming messages
 while True:
-  l = ser.readline()
-  if l != b"":
-    s = l.decode("ascii")
-    print(s, end = "")
+  # Received message?
+  s = readLineFromSerial(ser)
+  if re.match(r'^\[RSS: ', s):
+    print("\r\nDevice: {}".format(s))
+    message_from = s.split("<")[1].split(">")[0]
+    message_body = s[slice(s.find(">") + 2, len(s))]
+    # message_rss  = s.split("]")[0].split(" ")[1]
+    # message_snr  = s.split("]")[1].split(" ")[2]
 
-    # Received message?
-    if s.startswith("<"):
-      # Update last heard
-      sender  = s.split("<")[1].split(">")[0]
-      message = s.split(">")[1].strip()
-      print("Got message [" + message + "]")
-      lastHeard[sender] = datetime.now().strftime("%Y-%m-%d %H:%M")
-      print("Updated last heard for [" + sender + "] to " + lastHeard[sender])
+    # Update last heard
+    print("Got message [{}] from [{}]".format(message_body, message_from))
+    lastHeard[message_from] = datetime.now().strftime("%Y-%m-%d %H:%M")
+    print("Updated last heard for [{}] to {}".format(message_from, lastHeard[message_from]))
 
-      # WSBBS-HEARD command?
-      if "WSBBS-HEARD".lower() in s.lower():
-        print("Got WSBBS-HEARD command")
-        for sender in lastHeard:
-          writeToSerial("I last heard ")
-          writeToSerial(sender)
-          writeToSerial(" at ")
-          writeToSerial(lastHeard[sender])
-          writeToSerial("\r\n")
+    # HEARD command?
+    if "{} HEARD".format(bbs_name).lower() in s.lower():
+      print("Got HEARD command")
+      for senderName in lastHeard:
+        writeToSerial(ser, "I last heard <{}> at {}".format(senderName, lastHeard[senderName]))
 
-      # WSBBS-WRITE command?
-      elif "WSBBS-WRITE".lower() in s.lower():
-        print("Got WSBBS-WRITE command")
-        tokens = message.strip().split(" ")
+    # WRITE command?
+    elif "{} WRITE".format(bbs_name).lower() in s.lower():
+      print("Got WRITE command")
+      tokens = message_body.strip().split(" ")
+      del tokens[0]
+      for i in range(0, len(bbs_name.split(" "))):
         del tokens[0]
-        post = (" ".join(tokens)).strip()
-        print ("Post [" + post + "]")
-        if post != "":
-          wall.append("[" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "] <" + sender + "> " + post)
-          writeToSerial(sender)
-          writeToSerial(", I added your message to the wall.\r\n")
+      post = (" ".join(tokens)).strip()
+      print ("Received wall message [" + post + "]")
+      if post != "":
+        wall.append("[" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "] <" + message_from + "> " + post)
+        writeToSerial(ser, "{}, I have added your message to the wall.".format(message_from))
 
-          print("Wall contents now:")
-          for message in wall:
-            print(message)
+        # Maintain maximum wall length
+        if len(wall) > max_wall_messages:
+          print("Removing a message from the wall")
+          del wall[0]
+
+        print("Wall contents now:")
+        for message in wall:
+          print(message)
+      else:
+        writeToSerial(ser, "{}, what is your message?".format(message_from))
+
+    # READ command?
+    elif "{} READ".format(bbs_name).lower() in s.lower():
+      print("Got READ command")
+      if len(wall) > 0:
+        if len(wall) == 1:
+          writeToSerial(ser, "{}, there is one message on the wall.".format(message_from))
         else:
-          writeToSerial(sender)
-          writeToSerial(", what is your message?\r\n")
+          writeToSerial(ser, "{}, there are {} messages on the wall.".format(message_from, len(wall)))
 
-      # WSBBS-READ command?
-      elif "WSBBS-READ".lower() in s.lower():
-        print("Got WSBBS-READ command")
-        if len(wall) > 0:
-          for message in wall:
-            writeToSerial(message)
-            writeToSerial("\r\n")
-        else:
-          writeToSerial(sender)
-          writeToSerial(", there are no messages on the wall.\r\n")
+        for message in wall:
+          writeToSerial(ser, message)
+      else:
+        writeToSerial(ser, "{}, there are no messages on the wall.".format(message_from))
 
-      # WSBBS-HELP command?
-      elif "WSBBS-HELP".lower() in s.lower():
-        print("Got WSBBS-HELP command")
-        writeToSerial("Say WSBBS-READ to read messages on the wall. Say WSBBS-WRITE to write a message on the wall. Say WSBBS-HEARD to get a list of recently heard users.\r\n")
+    # HELP command?
+    elif "{} HELP".format(bbs_name).lower() in s.lower():
+      print("Got HELP command")
+      writeToSerial(ser, "Say {} READ to read messages on the wall. Say {} WRITE to write a message on the wall. Say {} HEARD to get a list of recently heard users.".format(bbs_name, bbs_name, bbs_name))
+
+    # TODO: Unknown command?
 
   # Time to send announcement?
   secondsUntilAnnounce = (nextAnnounce - datetime.now()).total_seconds()
   if secondsUntilAnnounce <= 0:
     nextAnnounce = datetime.now() + timedelta(seconds = announceSeconds)
     print("Sending announcement")
-    writeToSerial("Hello from WaveShark BBS! Say WSBBS-HELP to get a list of available commands.\r\n")
+    writeToSerial(ser, "Hello from {}! Say {} HELP to get a list of available commands.".format(bbs_name, bbs_name), 2)
 
 ser.close()
